afafb7491bfac64102a6711a9a0518bc68b59ef7
Leah Godfrey <missbabelfish@gmail.com>
2022-08-10 12:20:51 -0700

    good grief this video was grueling. such braining

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo');
@@ -23,13 +24,25 @@ const app = express()
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
+// Method override
+app.use(
+    methodOverride(function (req, res) {
+      if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        // look in urlencoded POST bodies and delete it
+        let method = req.body._method
+        delete req.body._method
+        return method
+      }
+    })
+)
+
 // Logging requests
 if (process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'))
 }
 
 // Handlebars helper
-const { formatDate, stripTags, truncate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon, select } = require('./helpers/hbs')
 
 // Handlebars view engine
 app.engine(
@@ -39,6 +52,8 @@ app.engine(
             formatDate,
             stripTags,
             truncate,
+            editIcon,
+            select,
         }, 
         defaultLayout: 'main', 
         extname: '.hbs'})
@@ -56,6 +71,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global variable
+app.use(function (req, res, next) {
+    res.locals.user = req.user || null
+    next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
